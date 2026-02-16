9ad5d002cc748e0fb5ca45a7ab12f3c107ab629b
Suhani Dhar <suhanidhar.2504@gmail.com>
2026-02-16 18:02:34 +0530

    deployed backend

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/expenses", expenseRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
